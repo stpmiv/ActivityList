2c2d17e9821bd769510c7f65e17a94351fc98b07
Sathaporn Chumwong <mivstp3912@gmail.com>
2019-04-06 18:43:12 +0700

    save060462

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,56 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { ActivityPage } from '../activity/activity';
+import { NewactivityPage } from '../newactivity/newactivity';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  addedToDoList:any[]=[];
+  tempTodo:any='';
+  todopage = NewactivityPage;
+  constructor(public navCtrl: NavController,public navParams:NavParams,public storage:Storage) {
+    this.storage.get('todoDetail').then(
+      (val)=>{
+        this.addedToDoList=val;
+      }
+    );
+  }
+  testObj = {
+    "title":"",
+    "description":""
+  };
 
-  constructor(public navCtrl: NavController) {
-
+  ngOninit():void{
+  this.storage.get('todoDetail').then(
+    (val)=>{
+      this.addedToDoList=val;
+    }
+  );
   }
 
+  ionViewillEnter(){
+  this.storage.get('todoDetail').then(
+    (val)=>{
+      this.addedToDoList=val;
+    }
+  );
+
 }
+
+  goToAddTodo():void{
+  this.navCtrl.push(NewactivityPage);
+
+}
+  removeItem(todo):void{
+  let index = this.addedToDoList.indexOf(todo);
+  if(index>-1){
+    this.addedToDoList.splice(index,1);
+    this.storage.set('todoDetails',this.addedToDoList);
+  }
+}
+}
+
